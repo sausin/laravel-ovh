785cd0930c585e9ad37bf01a619aaf0ae28b467a
sausin <sausin@users.noreply.github.com>
2020-01-10 00:03:18 +0530

    Support for keystone v3 in OVH

--- src/OVHServiceProvider.php
+++ src/OVHServiceProvider.php
@@ -2,15 +2,12 @@
 
 namespace Sausin\LaravelOvh;
 
+
+use OpenStack\OpenStack;
 use BadMethodCallException;
-use GuzzleHttp\Client;
-use GuzzleHttp\HandlerStack;
+use League\Flysystem\Filesystem;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\ServiceProvider;
-use League\Flysystem\Filesystem;
-use OpenStack\Common\Transport\Utils as TransportUtils;
-use OpenStack\Identity\v2\Service;
-use OpenStack\OpenStack;
 
 class OVHServiceProvider extends ServiceProvider
 {
@@ -44,7 +41,7 @@ class OVHServiceProvider extends ServiceProvider
     protected function checkConfig($config)
     {
         // needed keys
-        $needKeys = ['server', 'region', 'user', 'pass', 'tenantName', 'projectId', 'container'];
+        $needKeys = ['server', 'region', 'user', 'pass', 'userDomain', 'projectId', 'container'];
 
         if (count(array_intersect($needKeys, array_keys($config))) === count($needKeys)) {
             return;
@@ -62,21 +59,20 @@ class OVHServiceProvider extends ServiceProvider
      */
     protected function makeClient($config)
     {
-        // this is needed because default setup of openstack leads to authentication
-        // going to wrong path of the auth url as OVH uses deprecated version
-        $httpClient = new Client([
-            'base_uri' => TransportUtils::normalizeUrl($config['server']),
-            'handler'  => HandlerStack::create(),
-        ]);
-
-        // setup the client for OpenStack v1
+        // setup the client for OpenStack
         return new OpenStack([
             'authUrl' => $config['server'],
             'region' => $config['region'],
-            'username' => $config['user'],
-            'password' => $config['pass'],
-            'tenantName' => $config['tenantName'],
-            'identityService' => Service::factory($httpClient),
+            'user' => [
+                'name' => $config['user'],
+                'password' => $config['pass'],
+                'domain' => ['name' => 'userDomain'],
+            ],
+            'scope' => [
+                'project' => [
+                    'id' => $config['projectId']
+                ]
+            ],
         ]);
     }
 
